63d3678a60030a43014d7e22186c30c1f83d36d6
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2022-06-24 00:20:20 +0200

    init

---
+++ backend/src/admin/app.js
@@ -0,0 +1,14 @@
+export default {
+  config: {
+    locales: [
+      'de',
+    ],
+    auth: {
+      logo: './extensions/webqube_icon.svg'
+    }
+
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
