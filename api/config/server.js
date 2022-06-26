7f4dbdfb06bf2eb3427e25347eef2670ca08fa97
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2022-06-26 23:19:40 +0200

    init

--- api/config/server.js
+++ api/config/server.js
@@ -4,6 +4,6 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://backend.webqube.de'),
+  // url: env('PUBLIC_URL', 'https://backend.webqube.de'),
   // proxy: env.bool('IS_PROXIED', true),
 });
