ca6c1e8781adb5960450d0688e4a771bb21e351c
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2022-06-27 20:15:30 +0200

    init

--- api/config/server.js
+++ api/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST'),
+  port: env.int('PORT'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL'),
+  // url: env('PUBLIC_URL'),
   // proxy: env.bool('IS_PROXIED', true),
 });
